8deadfdd216b8f40cf97a749fb9c63269b1fa9b7
boris <boris.lok@cienet.com>
2023-05-23 16:36:28 +0800

    [Refactoring] Change the code style.

---
+++ lib/player.go
@@ -0,0 +1,32 @@
+package lib
+
+type UserId string
+
+type PlayerStatus int
+
+const (
+  Alive PlayerStatus = iota
+  Die
+)
+
+type Role int
+
+const (
+  Wolf Role = iota
+  Witch
+  Seer
+  Hunter
+  Civilian
+)
+
+type Player struct {
+  Id UserId
+  Name string
+  Role Role
+  Status PlayerStatus
+}
+
+// New create a player
+func New(id UserId, name string, role Role) *Player {
+  return &Player{ Id: id, Name: name, Status: Alive }
+}
--- main.go
+++ main.go
@@ -1,4 +1,14 @@
 package main
 
+import (
+	"fmt"
+	"math/rand"
+	"time"
+	"werewolves/lib"
+)
+
 func main() {
+  roles := []lib.Role{lib.Wolf, lib.Wolf, lib.Seer, lib.Witch, lib.Civilian, lib.Civilian}
+  lib.Shuffle(roles, rand.NewSource(time.Now().UnixNano()))
+  fmt.Printf("%v", roles)
 }
